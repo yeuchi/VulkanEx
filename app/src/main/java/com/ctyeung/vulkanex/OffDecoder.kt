7b5a812d4b4c36ffe1a561e80142dac6d5f13cd8
Chi Yeung <yeun0007@umn.edu>
2022-09-02 07:42:44 -0500

    parse - work in progress
    1. try-catch on all parsing functions.
    2. filter each line.

--- app/src/main/java/com/ctyeung/vulkanex/OffDecoder.kt
+++ app/src/main/java/com/ctyeung/vulkanex/OffDecoder.kt
@@ -36,83 +36,85 @@ class OffDecoder {
             return _vertices
         }
 
+    private fun preloadInit() {
+        _numEdges = 0
+        _numVertices = 0
+        _numFaces = 0
+    }
+
     fun loadFrom(uri: Uri, contentResolver: ContentResolver): Boolean {
         val `in`: InputStream? = contentResolver.openInputStream(uri)
         val r = BufferedReader(InputStreamReader(`in`))
 
-        // 1st line optional OFF
-        if (!validateOFFHeader(r)) {
-            return false
+        try {
+            preloadInit()
+            parseOFFHeader(r)
+            parseMeshInfo(r)
+            parseVertices(r)
+        } catch (e: Exception) {
+            Log.e("OffDecoder", e.toString())
         }
-        if (!parseMeshInfo(r)) {
-            return false
-        }
-        if (!parseVertices(r)) {
-            return false
-        }
-
-        /*
-         * TODO parse face, color, etc
-         */
         return true
     }
 
-    private fun removeComment(string: String): String {
-        val list = string.split('#')
-        return list.get(0)
+    private fun removeComment(string: String?): String? {
+        string?.split('#')?.apply {
+            return this[0]
+        }
+        return null
     }
 
     /*
      * 1st line is OFF (optional)
      */
-    private fun validateOFFHeader(r: BufferedReader): Boolean {
-        val line1 = r.readLine()
-        return if (line1.contains("OFF")) {
-            true
-        } else {
-            false
+    private fun parseOFFHeader(r: BufferedReader): Boolean {
+        var line: String?
+        while (r.readLine().also { line = it } != null) {
+            val filtered = removeComment(line)
+            return filtered?.contains("OFF") ?: throw Exception("missing OFF header")
         }
+        throw Exception("comments only")
     }
 
     /*
      * 2nd line: the number of vertices, number of faces, and number of edges
      */
     private fun parseMeshInfo(r: BufferedReader): Boolean {
-        val line2 = r.readLine()
-        val metrics = line2.split(' ')
-        if (metrics.size == 3) {
-            _numVertices = metrics.get(0).toIntOrNull() ?: return false
-            _numFaces = metrics.get(1).toIntOrNull() ?: return false
-            _numEdges = metrics.get(2).toIntOrNull() ?: return false
-            return true
+        var line: String?
+        while (r.readLine().also { line = it } != null) {
+            removeComment(line)?.let {
+                val metrics = it.split(' ')
+                if (metrics.size == 3) {
+                    _numVertices =
+                        metrics.get(0).toIntOrNull() ?: throw Exception("missing num verticies")
+                    _numFaces = metrics.get(1).toIntOrNull() ?: throw Exception("missing num faces")
+                    _numEdges = metrics.get(2).toIntOrNull() ?: throw Exception("missing num edges")
+                    return true
+                }
+            }
         }
-        return false
+        throw Exception("missing metric info")
     }
 
     private fun parseVertices(r: BufferedReader): Boolean {
-        try {
-            _vertices = FloatArray(_numVertices + 1)
-            _vertices?.apply {
+        _vertices = FloatArray(_numVertices)
+        _vertices?.apply {
 
-                var index = 0
-                var line: String?
-                while (r.readLine().also { line = it } != null &&
-                    index < _numVertices) {
-                    line?.let {
-                        /*
-                         * TODO Filter comment #
-                         */
-                        val xyz = it.split(' ')
-                        for (i in 0..2) {
-                            this[index++] = xyz.get(i).toFloat()
-                        }
+            var index = 0
+            var line: String?
+            while (r.readLine().also { line = it } != null &&
+                index < _numVertices) {
+                val filtered = removeComment(line)
+                filtered?.let {
+                    val xyz = it.split(' ')
+                    for (i in 0..2) {
+                        this[index] = xyz.get(i).toFloat()
+                        index++
                     }
                 }
-                return true
             }
-        } catch (e: Exception) {
-            Log.e("OffDecoder", e.toString())
+            return true
         }
-        return false
+        throw Exception("failed allocation")
     }
 }
